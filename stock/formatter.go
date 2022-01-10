e02c37b6677b7b9a7ebaa76049d9b5e36647f56d
Rifan Setiawan <rifan.setiawan@paper.id>
2022-01-10 17:10:14 +0700

    ts

--- stock/formatter.go
+++ stock/formatter.go
@@ -9,7 +9,7 @@ type StockFormatter struct {
 	High        float64 `json:"high"`
 	Low         float64 `json:"low"`
 	Vol         string  `json:"vol"`
-	Date        string  `json:"date"`
+	Date        string  `json:"timestamp"`
 	ChangeValue float64 `json:"change_value"`
 }
 
